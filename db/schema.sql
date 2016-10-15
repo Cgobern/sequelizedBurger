94521a335a1acabaaef4660319610092355dca33
cgobern <cgobern@gmail.com>
2016-10-15 16:41:22 -0400

    major push, db connectionnot working

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE `burgers_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE TABLE `burgers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `burger_name` varchar(255) NOT NULL,
+  `devoured` tinyint(4) DEFAULT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO `burgers` VALUES (1,'Cheeseburger',0,'2016-09-28 22:54:08'),
+INSERT INTO `burgers` VALUES (2,'Triple Cheeseburger',0,'2016-09-28 22:54:24'),
+INSERT INTO `burgers` VALUES (3,'Lamb Burger',0,'2016-09-28 22:54:41'),
+INSERT INTO `burgers` VALUES (4,'Mushroom Swiss Burger',0,'2016-09-28 23:04:09'),
+INSERT INTO `burgers` VALUES (5,'Bacon Double CheeseBurger',0,'2016-09-28 23:05:15');
